a47cea6c100845116b092303fecfe53b3756c29f
ShiChao1996 <2483061998@qq.com>
2017-11-06 10:50:51 +0800

    add cache to save articleList && welcome page display

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,49 +1,56 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import List from '../components/list';
 import {
-    Card,
-    Affix,
-    Button
+  Card,
+  Affix,
+  Button
 } from 'antd';
 import Side from '../components/sider';
 import './home.css';
 import Header from '../components/header';
 import Canvas from './animateBackground';
+import Cache from '../cache/cache';
+import Http from '../utils/http';
 
-import Http  from '../utils/http';
-
-export default class HomePage extends Component{
-    constructor(props){
-        super(props);
-        this.state = {
-            articleList: []
-        }
+export default class HomePage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      articleList: []
     }
+  }
 
-    componentWillMount(){
-        Http.Get(Http.url('article/getall'), (res) => {
-            if(res.status === 0){
-                this.setState({
-                  articleList: res.resp
-                })
-            }
-        }, (err) => {
-            console.log(err)
+  componentWillMount() {
+    if (Cache.exist('articleList')) {
+      this.setState({
+        articleList: Cache.getCache('articleList')
+      });
+      return;
+    }
+    Http.Get(Http.url('article/getall'), (res) => {
+      if (res.status === 0) {
+        Cache.saveList('articleList', res.resp);
+        this.setState({
+          articleList: res.resp
         })
-    }
+      }
+    }, (err) => {
+      console.log(err)
+    })
+  }
 
-    render(){
-        return(
-            <div className="body">
-                <Header />
-                <Canvas />
-                <div className='content'>
-                    <Card>
-                        { List(this.state.articleList, ) }
-                    </Card>
-                </div>
+  render() {
+    return (
+      <div className="body">
+        <Header/>
+        <Canvas/>
+        <div className='content'>
+          <Card>
+            {List(this.state.articleList,)}
+          </Card>
+        </div>
 
-                {/*<div className="bodyLeft">
+        {/*<div className="bodyLeft">
                     <Affix>
                         <div className='box'>
                             { Side('Lovae', '杨柳岸，晓风残月') }
@@ -54,10 +61,10 @@ export default class HomePage extends Component{
                     <p>© 2017 ♥ ShiChao</p>
                     <span>Powered by Lovae | 2017</span>
                 </div>*/}
-                <div className='footer'>
-                    Made by Lovae | 2017
-                </div>
-            </div>
-        )
-    }
+        <div className='footer'>
+          Made by Lovae | 2017
+        </div>
+      </div>
+    )
+  }
 }
